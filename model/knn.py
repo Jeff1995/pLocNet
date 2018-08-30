fe297eabc8619645e5f28b981c591acab2af7f50
Zhijie Cao <caozj@mail.cbi.pku.edu.cn>
2018-08-30 21:13:40 +0800

    Adapt knn

--- model/knn.py
+++ model/knn.py
@@ -1,88 +1,86 @@
-
+#!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
+import os
+import argparse
 import numpy as np
 import pandas as pd
-
-import pickle as pkl
+from scipy.sparse import coo_matrix
 import h5py
 
-from math import sqrt
-from sklearn.metrics import roc_auc_score
+from run_gcn import read_data
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-g", dest="g", type=str, required=True)
+parser.add_argument("-y", dest="y", type=str, required=True)
+parser.add_argument("--split", dest="split", type=str, required=True)
+parser.add_argument("-o", dest="output_path", type=str, required=True)
+cmd_args = parser.parse_args()
 
-f=open('../data/supervised_data/adj.pkl','rb')
-adj_coo=pkl.load(f)
-f.close()
+if not os.path.exists(cmd_args.output_path):
+    os.makedirs(cmd_args.output_path)
 
-f=open('..data/supervised_data/mask.pkl','rb')
-train_mask,val_mask,test_mask=pkl.load(f)
-f.close()
+print("Reading data...")
+data, adj_coo, train_mask, val_mask, test_mask = \
+    read_data(None, cmd_args.y, cmd_args.g, cmd_args.split)
+adj_coo = coo_matrix(adj_coo)
+n = data.size()
+localization = pd.DataFrame(
+    data["y"], index=data["protein_id"])
 
-f=open('..data/supervised_data/sample_id.pkl','rb')
-sample=pkl.load(f)
-f.close()
-
-n=len(sample)
-
-
-f=h5py.File("./pLocNet/data/preprocessed/localization.h5", "r")
-localization=pd.DataFrame(f["mat"].value,
-                          index=f["protein_id"].value,
-                          columns=f["label"].value)
-f.close()
-
-localization.index=[str(s, encoding='utf-8') for s in localization.index]
-localization.columns=[str(s, encoding='utf-8') for s in localization.columns]
-localization_rv = localization.loc[sample]
-
-
-neighborhood=dict()
+print("")
+neighborhood = dict()
 for i in range(n):
-    neighborhood[i]=[]
+    neighborhood[i] = []
 
 for i in range(adj_coo.row.shape[0]):
     neighborhood[adj_coo.row[i]].append(adj_coo.col[i])
 
-neighborhood_test=dict()
+neighborhood_train = dict()
 for i in neighborhood:
-    if test_mask[i]:
-        neighborhood_test[i]=[]
-        for j in neighborhood[i]:
-            if train_mask[j]:
-                neighborhood_test[i].append(j)
+    neighborhood_train[i] = []
+    for j in neighborhood[i]:
+        if train_mask[j]:
+            neighborhood_train[i].append(j)
 
-def knn_label(loc):
+def knn_label(loc, mask):
     labels = np.zeros([n])
     for i in range(n):
-        if test_mask[i]:
-            if len(neighborhood_test[i])>0:
-                labels[i] = len([j for j in neighborhood_test[i] if localization_rv.loc[sample[j],loc]])/len(neighborhood_test[i])
-            else:
-                labels[i] = sum(localization_rv.loc[train_mask,loc]/sum(train_mask)
-    return(labels)
+        if not mask[i]:
+            continue
+        if neighborhood_train[i]:
+            labels[i] = len([
+                j for j in neighborhood_train[i] if
+                localization.iloc[j, loc]
+            ]) / len(neighborhood_train[i])
+        else:
+            labels[i] = sum(
+                localization.iloc[train_mask, loc]
+            ) / sum(train_mask)
+    return labels
 
 
-def evaluate(labels,preds):
-    auc=roc_auc_score(labels,preds)
-    
-    TP=sum(np.round(preds,0)*labels)
-    FP=sum(np.round(preds,0)*(1-labels))
-    TN=sum((1-np.round(preds,0))*(1-labels))
-    FN=sum((1-np.round(preds,0))*labels)
-    
-    acc=(TP+TN)/(TP+FP+TN+FN)
-    se=TP/(TP+FN)
-    sp=TN/(TN+FP)
-    pre=TP/(TP+FP)
-    mcc=(TP*TN-FP*FN)/sqrt((TP+FP)*(TP+FN)*(TN+FP)*(TN+FN))
-    
-    print('AUC = ',auc,' ACC = ',acc,' SE = ',se,' SP = ',sp,' PRE = ',pre, ' MCC = ', mcc)
+# Training set
+print("Predicting training set...")
+train_pred = []
+for loc in localization.columns:
+    train_pred.append(knn_label(loc, train_mask)[test_mask])
+train_pred = np.stack(train_pred, axis=0).T
+
+# Test set
+print("Predicting test set...")
+train_pred = []
+test_pred = []
+for loc in localization.columns:
+    test_pred.append(knn_label(loc, test_mask)[test_mask])
+test_pred = np.stack(test_pred, axis=0).T
 
 
-for loc in localization_rv.columns:
-    print(loc+' results')
-    evaluate(localization_rv[loc].iloc[test_mask] ,knn_label(loc)[test_mask])
+with h5py.File(os.path.join(cmd_args.output_path, "result.h5")) as f:
+    g = f.create_group("train")
+    g.create_dataset("true", data=data["y"][train_mask])
+    g.create_dataset("pred", data=train_pred)
+    g = f.create_group("test")
+    g.create_dataset("true", data=data["y"][test_mask])
+    g.create_dataset("pred", data=test_pred)
