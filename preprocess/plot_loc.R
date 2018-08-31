2883f63780276cfec90320e013c6e2a7bd499165
Zhijie Cao <caozj@mail.cbi.pku.edu.cn>
2018-08-31 22:54:12 +0800

    Add a short version of loc.pdf

--- preprocess/plot_loc.R
+++ preprocess/plot_loc.R
@@ -15,8 +15,18 @@ loc_df_clean <- loc_df %>%
 loc_occurence <- as.data.frame(table(loc_df_clean$localization))
 colnames(loc_occurence) <- c("localization", "count")
 loc_occurence <- loc_occurence[order(loc_occurence$count, decreasing = TRUE), ]
+
 gp <- ggplot(loc_occurence, aes(x = localization, y = count)) +
-    geom_bar(stat = "identity") +
-    scale_x_discrete(limits = loc_occurence$localization) +
-    coord_flip()
+        geom_bar(stat = "identity") +
+            scale_x_discrete(limits = loc_occurence$localization) +
+                coord_flip()
 ggsave("../data/summary/loc.pdf", gp, height = 30)
+
+gp <- ggplot(loc_occurence[1:20, ], aes(x = localization, y = count)) +
+    geom_bar(stat = "identity") +
+    scale_x_discrete(limits = loc_occurence$localization[1:20]) +
+    coord_flip() + theme(
+        axis.text.y = element_text(size = 10),
+        axis.text.x = element_text(size = 12)
+    )
+ggsave("../data/summary/loc_short.pdf", gp)
